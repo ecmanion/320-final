bc28ace7a67c44e5b7387dad95b2e837dfd944be
ecmanion <89819521+ecmanion@users.noreply.github.com>
2022-11-25 19:36:10 -0500

    Add files via upload

---
+++ public/app/app.js
@@ -0,0 +1,729 @@
+var LISTS = [
+    {
+    name: "Candy",
+    listItems:[
+        {
+            name: "Oreos",
+            checked: false
+        },
+        {
+            name: "Sour Patch Kids",
+            checked: false
+        },
+        {
+            name: "KitKat",
+            checked: false
+        },
+        {
+            name: "Snickers",
+            checked: false
+        },
+        {
+            name: "Twix",
+            checked: false
+        },
+        {
+            name: "Twizlers",
+            checked: false
+        },
+        {
+            name: "Hersheys",
+            checked: false
+        },
+        {
+            name: "Milk Duds",
+            checked: false
+        },
+        {
+            name: "Mike n Ikes",
+            checked: false
+        },
+        {
+            name: "Milkyway",
+            checked: false
+        }
+
+        ],
+    },
+
+    {
+    name: "Groceries",
+    listItems:[
+        {
+            name: "milk",
+            checked: false 
+        },
+        {
+            name: "cheese",
+            checked: false 
+        },
+        {
+            name: "almonds",
+            checked: false 
+        },
+        {
+            name: "oranges",
+            checked: false 
+        },
+        {
+            name: "potatoes",
+            checked: false 
+        },
+        {
+            name: "cereal",
+            checked: false 
+        },
+        {
+            name: "tomatoes",
+            checked: false 
+        },
+        {
+            name: "chocolate",
+            checked: false 
+        },
+        {
+            name: "vitamins",
+            checked: false 
+        }, 
+        {
+            name: "yogurt",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Cookie Types",
+    listItems:[
+        {
+            name: "chocolate chip",
+            checked: false 
+        },
+        {
+            name: "oatmeal raisin",
+            checked: false 
+        },
+        {
+            name: "gingerbread",
+            checked: false 
+        },
+        {
+            name: "sugar",
+            checked: false 
+        },
+        {
+            name: "no-bake",
+            checked: false 
+        },
+        {
+            name: "peanut butter",
+            checked: false 
+        },
+        {
+            name: "snickerdoodle",
+            checked: false 
+        },
+        {
+            name: "shortbread",
+            checked: false 
+        },
+        {
+            name: "double chocolate chip",
+            checked: false 
+        }, 
+        {
+            name: "fortune",
+            checked: false 
+        },
+        
+    ]
+    },
+
+     {
+    name: "Snacks",
+    listItems:[
+        {
+            name: "celery and peanut butter",
+            checked: false 
+        },
+        {
+            name: "apple and peanut butter",
+            checked: false 
+        },
+        {
+            name: "yogurt and granola",
+            checked: false 
+        },
+        {
+            name: "smoothie",
+            checked: false 
+        },
+        {
+            name: "crackers and hummus",
+            checked: false 
+        },
+        {
+            name: "chips and salsa",
+            checked: false 
+        },
+        {
+            name: "chips",
+            checked: false 
+        },
+        {
+            name: "trail mix",
+            checked: false 
+        },
+        {
+            name: "snack bar",
+            checked: false 
+        }, 
+        {
+            name: "popcorn",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Camping",
+    listItems:[
+        {
+            name: "tent",
+            checked: false 
+        },
+        {
+            name: "food",
+            checked: false 
+        },
+        {
+            name: "firewood",
+            checked: false 
+        },
+        {
+            name: "lantern",
+            checked: false 
+        },
+        {
+            name: "camper",
+            checked: false 
+        },
+        {
+            name: "cooler",
+            checked: false 
+        },
+        {
+            name: "cooking materials",
+            checked: false 
+        },
+        {
+            name: "radio",
+            checked: false 
+        },
+        {
+            name: "mugs",
+            checked: false 
+        }, 
+        {
+            name: "chairs",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Thanksgiving",
+    listItems:[
+        {
+            name: "green bean casserole",
+            checked: false 
+        },
+        {
+            name: "turkey",
+            checked: false 
+        },
+        {
+            name: "mashed potatoes",
+            checked: false 
+        },
+        {
+            name: "gravy",
+            checked: false 
+        },
+        {
+            name: "cranberry sauce",
+            checked: false 
+        },
+        {
+            name: "bread rolls",
+            checked: false 
+        },
+        {
+            name: "stuffing",
+            checked: false 
+        },
+        {
+            name: "pumpkin pie",
+            checked: false 
+        },
+        {
+            name: "pecan pie",
+            checked: false 
+        }, 
+        {
+            name: "vegetables",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Fruits",
+    listItems:[
+        {
+            name: "strawberry",
+            checked: false 
+        },
+        {
+            name: "blueberry",
+            checked: false 
+        },
+        {
+            name: "orange",
+            checked: false 
+        },
+        {
+            name: "raspberry",
+            checked: false 
+        },
+        {
+            name: "kiwi",
+            checked: false 
+        },
+        {
+            name: "banana",
+            checked: false 
+        },
+        {
+            name: "pomegranate",
+            checked: false 
+        },
+        {
+            name: "grape",
+            checked: false 
+        },
+        {
+            name: "apple",
+            checked: false 
+        }, 
+        {
+            name: "grapefruit",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Christmas",
+    listItems:[
+        {
+            name: "food",
+            checked: false 
+        },
+        {
+            name: "tree",
+            checked: false 
+        },
+        {
+            name: "presents",
+            checked: false 
+        },
+        {
+            name: "lights",
+            checked: false 
+        },
+        {
+            name: "candles",
+            checked: false 
+        },
+        {
+            name: "tableware",
+            checked: false 
+        },
+        {
+            name: "family",
+            checked: false 
+        },
+        {
+            name: "friends",
+            checked: false 
+        },
+        {
+            name: "eggnog",
+            checked: false 
+        }, 
+        {
+            name: "music",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Building Materials",
+    listItems:[
+        {
+            name: "hammer",
+            checked: false 
+        },
+        {
+            name: "screwdriver",
+            checked: false 
+        },
+        {
+            name: "drill",
+            checked: false 
+        },
+        {
+            name: "nails",
+            checked: false 
+        },
+        {
+            name: "wood",
+            checked: false 
+        },
+        {
+            name: "stud finder",
+            checked: false 
+        },
+        {
+            name: "screws",
+            checked: false 
+        },
+        {
+            name: "glue",
+            checked: false 
+        },
+        {
+            name: "tape measure",
+            checked: false 
+        }, 
+        {
+            name: "level",
+            checked: false 
+        },
+        
+    ],
+    },
+
+     {
+    name: "Cities",
+    listItems:[
+        {
+            name: "Indianapolis",
+            checked: false 
+        },
+        {
+            name: "Chicago",
+            checked: false 
+        },
+        {
+            name: "New York",
+            checked: false 
+        },
+        {
+            name: "Dallas",
+            checked: false 
+        },
+        {
+            name: "Los Angeles",
+            checked: false 
+        },
+        {
+            name: "Austin",
+            checked: false 
+        },
+        {
+            name: "Miami",
+            checked: false 
+        },
+        {
+            name: "Cincinnati",
+            checked: false 
+        },
+        {
+            name: "Houston",
+            checked: false 
+        }, 
+        {
+            name: "Philadelphia",
+            checked: false 
+        },
+        
+    ],
+    },
+   
+    
+];
+
+var _db = "";
+var userExists = false;
+var userFullName = "";
+var _userProfileInfo = {};
+
+
+function barNavigation (){
+    $(".bars").click(function(e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+
+    $(".links a").click(function (e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+}
+$(document).ready(function(){
+    barNavigation();
+});
+
+
+
+
+function itemChecked (element, listIndex, itemIndex){
+    
+    $(element).parent().toggleClass("strike");
+    let checkedValue = !_userProfileInfo.lists[listIndex].listItems[itemIndex].checked;
+    _userProfileInfo.lists[listIndex].listItems[itemIndex].checked = checkedValue; 
+    updateUserInfo(_userProfileInfo);
+}
+
+function addItem (listIndex){
+    let newItemName = $("#addItem").val();
+    let newItemObj = {
+        name: newItemName,
+        checked: false,
+    };
+    _userProfileInfo.lists[listIndex].listItems.push(newItemObj);
+    updateUserInfo(_userProfileInfo);
+    loadListItems(listIndex);
+}
+
+function deleteItem (listIndex, index){
+    _userProfileInfo.lists[listIndex].listItems.splice(index,1);
+    updateUserInfo(_userProfileInfo);
+    loadListItems(listIndex); 
+}
+
+function loadListItems (listIndex){
+    let listString = `<button onclick="loadLists()">Back</button> <ul>`;
+    $.each(_userProfileInfo.lists[listIndex].listItems, function (index, listItem){
+        listString += `<li id="${index}" class="${listItem.checked ? "strike": ""}"><input ${listItem.checked ? (checked = "checked") : ""} type="checkbox" id="${index}" name="" onclick="itemChecked(this, ${listIndex}, ${index})">
+        <span>${listItem.name}</span><button class="delete" onclick="deleteItem(${listIndex},${index})">Delete</button></li>`; 
+    });
+    listString += `</ul><div class="addItemInput"><input id="addItem" type="text"><button class="addButton"onclick="addItem(${listIndex})">Add Item</button></div>`;
+    $("#app").html(listString); 
+    
+}
+
+function loadLists (){
+    let listString = "<ul>";
+    $.each(_userProfileInfo.lists, function(index, list){
+    listString += `<li id="${index}" onclick="loadListItems(${index})">${list.name} <span class="right">Items: ${list.listItems.length}</span></li>`;
+    });
+    listString += "</ul>";
+    $("#app").html(listString);
+    
+}
+
+
+
+function initFirebase (){
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user){
+            _db = firebase.firestore(); 
+            console.log("auth change logged in");
+            if(user.displayName){
+                $(".name").html(user.displayName); 
+            }
+            $(".signOut").prop("disabled", false);
+            $(".loadLists").prop("disabled", false);
+            userExists = true;
+        }else{
+            _db = "";
+            _userProfileInfo = {};
+            $("#app").empty();
+            console.log("auth change logged out");
+            $(".name").html("");
+            $(".signOut").attr("disabled", true);
+            $(".loadLists").attr("disabled", true);
+            userExists = false;
+            userFullName = "";
+        }
+    });
+}
+
+function addMainList (){
+    let newListName = $("#listName").val();
+    let newListObj = {
+        name: newListName,
+        listItems: [],
+    };
+
+    _userProfileInfo.lists.push(newListObj);
+    updateUserInfo(_userProfileInfo);
+    loadLists();
+    $("#listName").val("");
+}
+
+function updateUserInfo(userObj){
+    let id = firebase.auth().currentUser.uid;
+    _db.collection("Users").doc(id).update(userObj).then(()=>{
+        console.log("updated doc");
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("update error " + errorMessage);
+      });
+}
+
+function signOut(){
+    firebase
+    .auth()
+    .signOut()
+    .then(() => {
+    // Signed in..
+    alert("signed out");
+  })
+  .catch((error) => {
+    // Signed out...
+    console.log("error signing out" + errorMessage);
+  }); 
+}
+
+function logIn(){
+    let email = $("#logEmail").val();
+    let password = $("#logPassword").val();
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    alert("logged in");
+    $("#logEmail").val("");
+    $("#logPassword").val("");
+    _db.collection("Users").doc(user.uid).get().then((doc)=>{
+        // console.log(doc.data());
+        _userProfileInfo = doc.data();
+        loadLists();
+        console.log("login userinfo ", _userProfileInfo);
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("logged in error" + errorMessage);
+      });
+
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("logged in error" + errorMessage);
+  });
+}
+
+function createAccount() {
+    let fName = $("#fName").val();
+    let lName = $("#lName").val();
+    let email = $("#email").val();
+    let password = $("#password").val();
+    let fullName = fName + " " + lName;
+    let userObj = {
+        firstName: fName,
+        lastName: lName,
+        email: email,
+        lists: [],
+    };
+    // console.log("create " + fName + " fName " + lName);
+// Firebase Code
+firebase
+.auth()
+.createUserWithEmailAndPassword(email, password)
+.then((userCredential) => {
+// Signed in 
+var user = userCredential.user;
+alert("created");
+
+firebase.auth()
+.currentUser.updateProfile({
+    displayName: fullName,
+});
+
+_db
+.collection("Users")
+.doc(user.uid)
+.set(userObj)
+.then((doc)=> {
+    console.log("doc added ");
+    _userProfileInfo = userObj;
+    console.log("create userinfo ", _userProfileInfo);
+})
+
+.catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+    console.log("create error " + errorMessage);
+    });
+
+userFullName = fullName;
+$(".name").html(userFullName);
+$("#fName").val("");
+$("#lName").val("");
+$("#email").val("");
+$("#password").val("");
+})
+.catch((error) => {
+var errorCode = error.code;
+var errorMessage = error.message;
+// ..
+console.log("create error " + errorMessage);
+});
+}
+
+function signInAnon(){
+    firebase
+    .auth()
+    .signInAnonymously()
+    .then(() => {
+    // Signed in..
+    console.log("signed in");
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log("error signing in " + errorMessage);
+  });
+}
+
+
+$(document).ready(function(){
+    try {
+        let app = firebase.app();
+        
+        initFirebase();
+    } catch (error){
+        console.log("error " + error);
+    }
+    
+});
